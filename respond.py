d349fac1b23065fb8ea515e76f387faed3647367
ynst <yunust418@gmail.com>
2025-10-02 00:20:22 -0700

    updated prompt + new model

--- respond.py
+++ respond.py
@@ -14,7 +14,9 @@ def prompt_llm(prompt):
     # This function allows us to prompt an LLM via the Together API
 
     # model
-    model = "meta-llama/Meta-Llama-3-8B-Instruct-Lite"
+    model = "openai/gpt-oss-20b"
+
+    print(f"Using {model}")
 
     # # Calculate the number of tokens
     # tokens = len(prompt.split())
@@ -37,11 +39,18 @@ def get_ai_response(user_message, chat_history, homework):
     - Make your answers at most 30 words
     - Only give the response to the message
     - Give the students hints or suggestions
-    - Do not solve the question for them
+    - Do not provide direct answers to homework questions
+    - If you don't know the answer, just say "I don't know"
+    - Do not make up answers
+    - Check with the student to see if they need further assistance or clarification
+    - Be friendly and encouraging
+    - If your last response included a question, wait for the student's reply before responding again
     - Please use the knowledge base to answer the question if relevant
 
     Knowledge base:
-    
+    - Homework assignments:
+    {homework}
+
     Error database:
 
     Respond to the user's message below:
@@ -49,9 +58,6 @@ def get_ai_response(user_message, chat_history, homework):
 
     Here is your chat history with the user:
     {chat_history}
-
-    Here is the user's Homework:
-    {homework}
     """
 
     return prompt_llm(prompt)
