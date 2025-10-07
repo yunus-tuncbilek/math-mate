11d140e3436cbe45073ef0dc41cfeace5ef967c4
ynst <yunust418@gmail.com>
2025-10-07 13:42:12 -0700

    fixing backslash error on HF

--- create_sample_data.py
+++ create_sample_data.py
@@ -16,5 +16,9 @@ def save_json(filename, data):
     os.replace(tmp, filename)
 
 save_json(USERS_FILE, json.loads(sample.sample_users))
-save_json(HOMEWORKS_FILE, json.loads(sample.sample_homeworks))
 save_json(INTERACTIONS_FILE, json.loads(sample.sample_interactions))
+
+# escape backslashes so LaTeX like \sum, \int don't produce invalid JSON escapes
+cleaned = sample.sample_homeworks.replace("\\", "\\\\")
+data = json.loads(cleaned)
+save_json(HOMEWORKS_FILE, data)
